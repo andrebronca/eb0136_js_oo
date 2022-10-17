7a0eff03a69aaca7a2b372c02d18b69cee583b14
André R. B <bronca.andre@gmail.com>
2022-10-17 20:16:12 -0300

    estudo de objetos

--- objects_cap04/objects_05.js
+++ objects_cap04/objects_05.js
@@ -1,2 +1,24 @@
 //pg 129
 //passing objects
+var origiginal = {howmany: 1};
+var mycopy = origiginal;
+console.log(mycopy.howmany);    //1
+mycopy.howmany = 100;
+console.log(mycopy.howmany);    //100
+
+var nullify = function(o){ o.howmany = 0; };
+nullify(origiginal);
+console.log(origiginal.howmany);    //0
+
+//comparing objects
+var fido = {breed: 'dog'};
+var benji = {breed: 'dog'};
+console.log( benji === fido );  //false
+console.log( benji == fido );   //false
+
+var mydog = benji;
+console.log( mydog === benji );     //true
+console.log( mydog == benji );      //true
+console.log( mydog === fido );      //false
+
+
